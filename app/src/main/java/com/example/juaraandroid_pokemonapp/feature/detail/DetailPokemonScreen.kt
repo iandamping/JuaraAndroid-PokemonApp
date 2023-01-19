49de36d74789e5ac5b47f18d0156027cc6eb3863
Ian Damping <ian.damping@spesolution.com>
2023-01-19 13:55:33 +0700

    done : 1. add error screen

--- app/src/main/java/com/example/juaraandroid_pokemonapp/feature/detail/DetailPokemonScreen.kt
+++ app/src/main/java/com/example/juaraandroid_pokemonapp/feature/detail/DetailPokemonScreen.kt
@@ -1,12 +1,19 @@
 package com.example.juaraandroid_pokemonapp.feature.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -22,7 +29,7 @@ fun DetailPokemonScreen(
     val state by viewModel.detailState.collectAsState()
 
     ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val (imageRef, spriteImageRef, bioRef, statSectionRef, typeSectionRef) = createRefs()
+        val (imageRef, spriteImageRef, bioRef, statSectionRef, typeSectionRef, errorHandlingRef) = createRefs()
         val imageGuideLine = createGuidelineFromTop(0.3f)
 
         when {
@@ -53,14 +60,15 @@ fun DetailPokemonScreen(
                         }, pokemonItem = state.data!!
                 )
 
-                DetailPokemonBioSection(modifier = Modifier
-                    .constrainAs(bioRef) {
-                        top.linkTo(spriteImageRef.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        width = Dimension.fillToConstraints
-                        height = Dimension.wrapContent
-                    }, pokemonItem = state.data!!
+                DetailPokemonBioSection(
+                    modifier = Modifier
+                        .constrainAs(bioRef) {
+                            top.linkTo(spriteImageRef.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.wrapContent
+                        }, pokemonItem = state.data!!
                 )
 
                 DetailPokemonStatSection(
@@ -86,7 +94,25 @@ fun DetailPokemonScreen(
                 )
             }
             state.failedMessage.isNotEmpty() -> {
+                Column(
+                    modifier = Modifier.constrainAs(errorHandlingRef) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier.fillMaxWidth(),
+                        painter = painterResource(id = R.drawable.error_image),
+                        contentDescription = stringResource(id = R.string.no_data_available)
+                    )
 
+                    Text(
+                        text = stringResource(id = R.string.no_data_available),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
             }
         }
 
--- app/src/main/java/com/example/juaraandroid_pokemonapp/feature/quiz/QuizPokemonScreen.kt
+++ app/src/main/java/com/example/juaraandroid_pokemonapp/feature/quiz/QuizPokemonScreen.kt
@@ -1,21 +1,24 @@
 package com.example.juaraandroid_pokemonapp.feature.quiz
 
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.juaraandroid_pokemonapp.R
 import com.example.juaraandroid_pokemonapp.feature.PokemonQuizViewModel
 
 @Composable
@@ -27,7 +30,7 @@ fun QuizPokemonScreen(
 
 
     ConstraintLayout(modifier = modifier.fillMaxSize()) {
-        val lazyRowRef = createRef()
+        val (lazyRowRef, errorHandlingRef) = createRefs()
         val scrollState = rememberLazyListState()
         when {
             state.data.isNotEmpty() -> {
@@ -60,7 +63,25 @@ fun QuizPokemonScreen(
                 }
             }
             state.failedMessage.isNotEmpty() -> {
+                Column(
+                    modifier = Modifier.constrainAs(errorHandlingRef) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier.fillMaxWidth(),
+                        painter = painterResource(id = R.drawable.error_image),
+                        contentDescription = stringResource(id = R.string.no_data_available)
+                    )
 
+                    Text(
+                        text = stringResource(id = R.string.no_data_available),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
             }
         }
 
